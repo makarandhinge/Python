fead9b6e7fd1a7bd5c85ded932d48baf11539b47
Makarand M Hinge <mmh2212002@gmail.com>
2023-08-28 10:00:20 +0530

    Python Arithmetic Operation in program

---
+++ Operators.py
@@ -0,0 +1,35 @@
+# Addition
+a = 5 + 3
+print("Addition:", a)  
+
+# Subtraction
+b = 10 - 4
+print("Subtraction:", b)  
+
+# Multiplication
+c = 6 * 2
+print("Multiplication:", c)  
+
+# Division
+d = 15 / 3
+print("Division:", d) 
+
+# Floor Division
+e = 17 // 4
+print("Floor Division:", e)  
+
+# Modulus (Remainder)
+f = 17 % 4
+print("Modulus:", f)  
+
+# Exponentiation
+g = 2 ** 3
+print("Exponentiation:", g)  
+
+# Negative
+h = -10
+print("Negative:", h)  
+
+# Positive (not commonly used, just for completeness)
+i = +7
+print("Positive:", i) 
