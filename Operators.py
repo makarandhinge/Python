12be8f7e50a4d580fbd0ffce8b98b595a2a68d9f
Makarand M Hinge <mmh2212002@gmail.com>
2023-08-28 10:13:12 +0530

    Add more lines of assignment operators using python

--- Operators.py
+++ Operators.py
@@ -33,3 +33,67 @@ print("Negative:", h)
 # Positive (not commonly used, just for completeness)
 i = +7
 print("Positive:", i) 
+
+# Assignment
+a = 5
+print("Assignment:", a)  
+
+# Addition Assignment
+b = 10
+b += 3
+print("Addition Assignment:", b) 
+
+# Subtraction Assignment
+c = 15
+c -= 7
+print("Subtraction Assignment:", c)  #
+
+# Multiplication Assignment
+d = 6
+d *= 4
+print("Multiplication Assignment:", d)  
+
+# Division Assignment
+e = 20
+e /= 5
+print("Division Assignment:", e)  
+
+# Floor Division Assignment
+f = 17
+f //= 3
+print("Floor Division Assignment:", f)  
+
+# Modulus Assignment
+g = 25
+g %= 7
+print("Modulus Assignment:", g)  
+
+# Exponentiation Assignment
+h = 3
+h **= 2
+print("Exponentiation Assignment:", h)  
+
+# Bitwise AND Assignment
+i = 0b101010
+i &= 0b110011
+print("Bitwise AND Assignment:", bin(i))  
+
+# Bitwise OR Assignment
+j = 0b101010
+j |= 0b110011
+print("Bitwise OR Assignment:", bin(j))  
+
+# Bitwise XOR Assignment
+k = 0b101010
+k ^= 0b110011
+print("Bitwise XOR Assignment:", bin(k))  
+
+# Bitwise Left Shift Assignment
+l = 0b101010
+l <<= 2
+print("Bitwise Left Shift Assignment:", bin(l)) 
+
+# Bitwise Right Shift Assignment
+m = 0b101010
+m >>= 1
+print("Bitwise Right Shift Assignment:", bin(m))  
